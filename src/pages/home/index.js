93c29a553ac69feb14c60829e809b771b21e0325
Helen Nguyen <hxn150@case.edu>
2023-09-25 21:51:04 -0400

    Edit 13: contact route

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -47,7 +47,7 @@ export const Home = () => {
                       <div className="ring three"></div>
                     </div>
                   </Link>
-                  <Link to="/portfolio">
+                  <Link to="/contact">
                     <div id="button_h" className="ac_btn btn">
                       Contact Me
                       <div className="ring one"></div>
